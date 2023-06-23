85fbdd274799f619af8e8a0978c80e064613b667
Matthew <mattbrenn7@gmail.com>
2023-06-23 00:17:24 -0400

    added more npcs

--- loki/src/SessionOptionsEnums.js
+++ loki/src/SessionOptionsEnums.js
@@ -1,4 +1,7 @@
 import ShuPic from "./npcImages/Shu.png";
+import MatthewPic from "./npcImages/Matthew.png";
+import ShahZaibPic from "./npcImages/ShahZaib.jpg";
+import LizziePic from "./npcImages/Lizzie.jpg";
 
 export const llmOptions = {
     GPT3: {name: "GPT 3", key: "GPT3"},
@@ -6,4 +9,7 @@ export const llmOptions = {
 
 export const npcOptions = {
     SHU: { "name": "Shu", "pic": ShuPic },
+    Matthew: { "name": "Matthew", "pic": MatthewPic },
+    ShahZaib: { "name": "ShahZaib", "pic": ShahZaibPic },
+    Lizzie: { "name": "Lizzie", "pic": LizziePic },
 }
