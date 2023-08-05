7112ee01d1fdf7f32c5020b2b985c276aee1c344
Idris <iawardere3@gmail.com>
2023-08-05 10:26:22 -0700

    New modules and character

--- loki/src/SessionOptionsEnums.js
+++ loki/src/SessionOptionsEnums.js
@@ -1,9 +1,11 @@
 import ShuPic from "./npcImages/Shu.jpg";
+import AttaccaPic from "./npcImages/orianna.jpg"
 
 export const llmOptions = {
     GPT3: {name: "GPT 3", key: "GPT3"},
 }
 
 export const npcOptions = {
-    Shu: { "name": "Shu", "key": "Shu", "pic": ShuPic }
+    Shu: { "name": "Shu", "key": "Shu", "pic": ShuPic },
+    Attacca: {"name": "Attacca", "key": "Attacca", "pic": AttaccaPic}
 }
